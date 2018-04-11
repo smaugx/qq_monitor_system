b10c4ddf691dd1d5b13187b80be20a5a3a32b0e7
menghong.jiao <menghong.jiao@upai.com>
2018-04-11 09:47:18 +0800

    delete private info

--- alive_qq.sh
+++ alive_qq.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-curl -XPOST -H"Content-Type: application/json" https://iruka.upyun.com/qqapi/upwarning/ -d '{"upid": '200', "content": "I am alive!"}'
+#mojo-webqq 的插件 openqq 提供了一个 http 接口，故可以利用它实现定时探测，以探测 qq 是否还在线
+
+curl -XPOST -H"Content-Type: application/json" https://yourself.com/qqapi/upwarning/ -d '{"upid": '200', "content": "I am alive!"}'
